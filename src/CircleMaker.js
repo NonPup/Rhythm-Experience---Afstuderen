fd41c623a4c1c8b8af780513e5a3396548db794e
Manon van den Berg <manon.vandenberg@student.fontys.nl>
2022-11-30 16:22:17 +0100

    Font Bebas Neue added and percentage score added
    Total amount of notes will be decided later its a placeholder right now

--- src/CircleMaker.js
+++ src/CircleMaker.js
@@ -9,21 +9,22 @@ export default class CircleMaker extends Phaser.GameObjects.Container
         this.scene.add.existing(this)
         this.score = 0;
         let r3;
-        // making gameobjects const onaanpasbaar, let of var wel
+        // making gameobjects, const onaanpasbaar, let of var wel
         const circle = this.scene.add.image(100, 200, 'circle');
         let RadiusCircle = 80;
         
-
+        //add circle
         r3 = this.scene.add.circle(100, 200, RadiusCircle);
-
+        //give circle line look
         r3.setStrokeStyle(3, 0xE85A95);
 
-        // add image to container
+        // add image and circle to container
         this.add([circle, r3])
 
         // set container position (also move image)
-        this.setPosition(Phaser.Math.Between(window.innerWidth), Phaser.Math.Between(window.innerHeight))
+        this.setPosition(Phaser.Math.Between(0, 215), Phaser.Math.Between(0, 429))
 
+        // on click event of circle image
         circle.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
             if (r3.radius <= 42){
                 this.score++;
@@ -31,6 +32,7 @@ export default class CircleMaker extends Phaser.GameObjects.Container
 
             console.log(`${this.score} ive been clicked!`)
 
+            // turn circle image and outline circle off
             r3.setVisible(false);
             r3.setActive(false);
             circle.setVisible(false);
@@ -38,7 +40,7 @@ export default class CircleMaker extends Phaser.GameObjects.Container
 
         },this);
 
-
+        // movement of outline circle
         const CircleTween = this.scene.tweens.add({
             targets: r3,
             radius: 34,
@@ -47,6 +49,7 @@ export default class CircleMaker extends Phaser.GameObjects.Container
             
         });
 
+        // turn circle image and outline circle off on complete animation
         CircleTween.on('complete',() => {
             r3.setVisible(false);
             r3.setActive(false);
@@ -56,6 +59,7 @@ export default class CircleMaker extends Phaser.GameObjects.Container
 
     }
     
+    // function to get the score
     GetScore(){
         return this.score
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import Circle from './assets/circle.png';
 import CircleMaker from './CircleMaker.js';
+import { WebFontLoaderPlugin } from 'phaser3-webfont-loader';
 
 let cirkelspawn;
 
@@ -12,21 +13,30 @@ class MyGame extends Phaser.Scene
         super();
     }
 
+    // load in assets
     preload ()
     {
         this.load.image('logo', logoImg);
         this.load.image('circle', Circle);
+        this.load.webfont('Bebas Neue', 'https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
     }
       
     create ()
     {
+        // creates circle for game through different class
         cirkelspawn = new CircleMaker(this, 10, 10);
-        this.ScoreGame = this.add.text(10, 10, cirkelspawn.GetScore(), { font: '30px Courier', fill: '#00ff00' });
+
+        // shows score on screen
+        this.ScoreGame = this.add.text(24, 32, cirkelspawn.GetScore(), { font: '28px Bebas Neue', fill: '#FFFFFF' });
+        this.PercentageGame = this.add.text(24, 65, cirkelspawn.GetScore()/200*100 + '%', { font: '24px Bebas Neue', fill: '#E85A95' });
     }
 
     update(){
         console.log(cirkelspawn.GetScore());
+
+        //updates the score to show the right amount on screen from class CircleMaker
         this.ScoreGame.setText(cirkelspawn.GetScore())
+        this.PercentageGame.setText(cirkelspawn.GetScore()/50*100 + '%')
     }
 }
 
@@ -35,7 +45,15 @@ const config = {
     parent: 'phaser-example',
     width: 375,
     height: 629,
-    scene: MyGame
+    scene: MyGame,
+    // webfont plugin
+    plugins: {
+        global: [{
+          key: 'WebFontLoader',
+          plugin: WebFontLoaderPlugin,
+          start: true
+        }]
+      }
 };
 
 const game = new Phaser.Game(config);
