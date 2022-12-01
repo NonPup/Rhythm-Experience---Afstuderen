4c3023f984bef6a0dd77fef7314e57d1ace4c0f6
Manon van den Berg <manon.vandenberg@student.fontys.nl>
2022-12-01 15:11:12 +0100

    score and circle click fixed
    score now shows and circle click works a and spawns

--- src/CircleMaker.js
+++ src/CircleMaker.js
@@ -5,11 +5,12 @@ export default class CircleMaker extends Phaser.GameObjects.Container
     constructor (scene, x, y, children)
     {
         super(scene, x, y, children);
-        this.score = 0;
         this.scene.add.existing(this)
 
         this.r3;
-        // making gameobjects, const onaanpasbaar, let of var wel
+        this.checkradius = false;
+
+        // making gameobjects
         this.circle = this.scene.add.image(100, 200, 'circle');
         let RadiusCircle = 80;
         
@@ -26,13 +27,18 @@ export default class CircleMaker extends Phaser.GameObjects.Container
 
         // on click event of circle image
         this.circle.setInteractive().on('pointerdown', function(pointer, localX, localY, event){
-            this.UpdateScore();
-            console.log(`${this.score} ive been clicked!`)
+            //checks if radius is less or equal to the radius of the other circle and makes the check true or false according to the answer
+            if (this.r3.radius <= 42){
+                this.checkradius = true;
+            }else {
+                this.checkradius = false;
+                 }
 
             // turn circle image and outline circle off
             this.DeleteCircle();
 
         },this);
+
 
         // movement of outline circle
         const CircleTween = this.scene.tweens.add({
@@ -55,16 +61,26 @@ export default class CircleMaker extends Phaser.GameObjects.Container
         return this.score
     }
 
-    UpdateScore(){
-        if (this.r3.radius <= 42){
-            this.score++;
-        }
+    ReturnRadius(){
+        return this.checkradius
     }
 
+    // UpdateScore(){
+    //     if (this.r3.radius <= 42){
+    //         this.score++;
+    //     }
+    // }
+
+    // hides circle
     DeleteCircle(){
         this.r3.setVisible(false);
         this.r3.setActive(false);
         this.circle.setVisible(false);
         this.circle.setActive(false);
+    }
+
+    // puts the check of the radius to false again
+    DisableRadius(){
+        this.checkradius = false;
     }
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { WebFontLoaderPlugin } from 'phaser3-webfont-loader';
 
 let cirkelspawn;
 let timedEvent;
-let score;
 
 class MyGame extends Phaser.Scene
 {
@@ -25,50 +24,34 @@ class MyGame extends Phaser.Scene
       
     create ()
     {
+        //score keeper
+        this.score = 0;
+
         // creates circle for game through different class
         cirkelspawn = new CircleMaker(this, 10, 10);
-        console.log('HELP')
+        //spawns the circles every delay, repeats certain amount of times
         timedEvent = this.time.addEvent({ delay: 1000, callback: () => {
             cirkelspawn = new CircleMaker(this, 10, 10);
-            console.log('KUTAPP')
         }, callbackScope: this, repeat: 20, repeatCount: 0});
-        console.log(timedEvent)
-
-        // var timeCheck;function CircleMaker() {
-        //     //do something
-        //     timeCheck = game.time.now;}
-
-        // for (let i = 0; i < 40; i++){
-        // delay = 1000;
-        // }
 
         // shows score on screen
-        this.ScoreGame = this.add.text(24, 32, cirkelspawn.GetScore(), { font: '28px Bebas Neue', fill: '#FFFFFF' });
-        this.PercentageGame = this.add.text(24, 65, cirkelspawn.GetScore()/200*100 + '%', { font: '24px Bebas Neue', fill: '#E85A95' });
+        this.ScoreGame = this.add.text(24, 32, this.score, { font: '28px Bebas Neue', fill: '#FFFFFF' });
+        //show percentage of how many you hit
+        this.PercentageGame = this.add.text(24, 65, (this.score/200*100).toFixed(0) + '%', { font: '24px Bebas Neue', fill: '#E85A95' });
         
     }
 
     update(){
-        console.log(cirkelspawn.GetScore());
-
+        //looks if the circle is clicked and under the radius, then adds score and disables the check to false again
+        if (cirkelspawn.ReturnRadius() == true){
+            this.score++;
+            cirkelspawn.DisableRadius();
+        }
         //updates the score to show the right amount on screen from class CircleMaker
-        this.ScoreGame.setText(cirkelspawn.GetScore())
-        this.PercentageGame.setText(cirkelspawn.GetScore()/50*100 + '%')
+        this.ScoreGame.setText(this.score)
+        this.PercentageGame.setText(this.score/50*100 + '%')
 
-        // // test for 3 second delay
-        // if (game.time.now - timeCheck > 3000){
-        //     //3 seconds have elapsed, so safe to do something
-        //     cirkelspawn;}
-            
-        // else{
-        //     //still waiting
-        //     }
     }
-
-    // onEvent (){
-    //     cirkelspawn = new CircleMaker(this, 10, 10);
-    //     console.log('KUTAPP')
-    //     }
 }
 
 const config = {
