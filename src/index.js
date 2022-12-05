c5642e9a18d63f0b0981a8b205a3929eaf3076b7
NonPon <manon.vandenberg@student.fontys.nl>
2022-12-05 16:33:06 +0100

    Added overlay, background video short version and little bottom top fade
    Score is broken

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
 import Circle from './assets/circle.png';
+import Bottom from './assets/bottom.png';
+import Top from './assets/top.png';
+import Overlay from './assets/overlay.png';
 import CircleMaker from './CircleMaker.js';
 import { WebFontLoaderPlugin } from 'phaser3-webfont-loader';
 //import CrackerIsland from './assets/crackerisland.mp4';
@@ -20,33 +23,45 @@ class MyGame extends Phaser.Scene
     {
         this.load.image('logo', logoImg);
         this.load.image('circle', Circle);
+        this.load.image('bottom', Bottom);
+        this.load.image('top', Top);
+        this.load.image('overlay', Overlay);
         this.load.webfont('Bebas Neue', 'https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
 
-        //this.load.video('wormhole', CrackerIsland, 'loadeddata', false, true);
+        this.load.video('wormhole', './src/assets/crackerisland-edit.mp4', 'loadeddata', false, false);
     }
       
     create ()
     {
-        //this.video = this.add.video(300, 200, 'wormhole');
+        let vid = this.video = this.add.video(180, 310, 'wormhole');
 
-        //this.video.play();
+        this.video.play();
+
+        //prevents video from pause when tab out
+        vid.setPaused(false);
 
         //score keeper
         this.score = 0;
 
+        //bit of a fade top and bottom
+        this.bottom = this.add.image(187, 550, 'bottom');
+        this.top = this.add.image(187, 80, 'top');
+
         // creates circle for game through different class
         cirkelspawn = new CircleMaker(this, 10, 10);
         //spawns the circles every delay, repeats certain amount of times
-        timedEvent = this.time.addEvent({ delay: 1000, callback: () => {
+        timedEvent = this.time.addEvent({ delay: 500, callback: () => {
             cirkelspawn = new CircleMaker(this, 10, 10);
         }, callbackScope: this, repeat: 20, repeatCount: 0});
 
-        
+        // seethrough overlay
+        this.overlay = this.add.image(187, 310, 'overlay');
+
         // shows score on screen
         this.ScoreGame = this.add.text(24, 32, parseInt(this.score), { font: '28px Bebas Neue', fill: '#FFFFFF' });
         //show percentage of how many you hit
         this.PercentageGame = this.add.text(24, 65, (parseInt(this.score)/200*100).toFixed(0) + '%', { font: '24px Bebas Neue', fill: '#E85A95' });
-        
+    
     }
 
     update(){
@@ -55,10 +70,12 @@ class MyGame extends Phaser.Scene
             this.score++;
             cirkelspawn.DisableRadius();
         }
+        
         //updates the score to show the right amount on screen from class CircleMaker
         this.ScoreGame.setText(this.score)
         this.PercentageGame.setText(this.score/50*100 + '%')
 
+        
     }
 }
 
